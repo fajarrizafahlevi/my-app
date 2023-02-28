ee4ff66fb1a8d83213269b0d29da52375116ab85
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-03-01 06:44:28 +0700

    edit about

--- src/Views/About.js
+++ src/Views/About.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="text-center">
+      <h2 className="font-bold text-xl m-3">About</h2>
+      <div className="border rounded m-3 p-3">
+        <p>This is basic app that can be function as a template for my app</p>
+        <span>&copy; 2023</span>
+      </div>
+    </div>
+  );
 }
 
 export default About;
