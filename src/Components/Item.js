7d4a4785b440f420aae33ee6a9e232a8ee75b102
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-03-01 09:16:56 +0700

    add detail to item

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-function Item({ id, title, deleteItem }) {
+function Item({ id, title, body, deleteItem }) {
   return (
-    <div className="m-3 p-3 border rounded flex justify-between items-center">
-      <h2 className="ml-2">{title}</h2>
-      <div>
-        <FontAwesomeIcon
-          icon={faDeleteLeft}
-          className="ml-3 cursor-pointer"
-          onClick={() => deleteItem(id)}
-        />
+    <div className="m-3 p-3 border rounded flex justify-between items-end">
+      <div className="flex-col">
+        <h2 className="font-bold">{title}</h2>
+        <p className="text-slate-500">{body}</p>
       </div>
+      <FontAwesomeIcon
+        icon={faTrash}
+        className="ml-3 cursor-pointer"
+        onClick={() => deleteItem(id)}
+      />
     </div>
   );
 }
